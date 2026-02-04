e7aa3a457e0a6903fad49d87997677f8d7ecfc30
Chamisae812 <anjihoon7777@gmail.com>
2026-02-04 17:45:03 +0900

    js 3번째 2차

--- 03-javascript/practice05-function-updown.js
+++ 03-javascript/practice05-function-updown.js
@@ -1,29 +1,61 @@
-function updownGame() {
-  const answer = Math.floor(Math.random() * 100 + 1);
+let result;
+let count = 0;
 
-  while (true) {
-    const input = prompt("1~100 중에 숫자를 입력해주세요.");
+//1~100
+function getTarget() {
+  return Math.floor(Math.random() * 100 + 1);
+}
 
-    if (input === null) {
-      alert("게임을 종료합니다.");
-      break;
-    }
+//input
+function setInput() {
+  input = prompt("1~100 중에 숫자를 입력해주세요.");
+  num = Number(input);
+}
 
-    const num = Number(input);
+//판단
+function judge(answer) {
+  if (input === null) return null;
 
-    if (input.trim() === "" || Number.isNaN(num)) {
-      alert("님 바보임? 제대로 입력하셈");
-    }
+  if (input.trim() === "" || Number.isNaN(num) || num < 1 || num > 100) {
+    return "error";
+  }
 
-    if (num === answer) {
-      alert("오.. 정답! 좀 치는데?");
-      break;
-    } else if (num < answer) {
-      alert("해당 숫자보다 큼");
-    } else {
-      alert("해당 숫자보다 작음");
-    }
+  count++;
+  //count는 수업후 추가 윗 부분은 잘못 입력한거기 때문에 그 다음 부터 카운트
+  if (num === answer) {
+    return "success";
+  } else if (num < answer) {
+    return "up";
+  } else {
+    return "down";
   }
 }
 
-updownGame();
+//showMessage
+function showMessage() {
+  if (result === "success") {
+    alert(`오.. 정답! ${count}번만에 맞췄어 좀 치는데?`);
+  } else if (result === "error") {
+    alert("님 바보임? 제대로 입력하셈");
+  } else if (result === "up") {
+    alert("해당 숫자보다 큼");
+  } else if (result === "down") {
+    alert("해당 숫자보다 작음");
+  }
+}
+
+//play
+function play() {
+  const answer = getTarget();
+
+  while (true) {
+    setInput();
+    result = judge(answer);
+    showMessage();
+
+    if (result === null || result === "success") break;
+    if (result === "error") continue;
+  }
+}
+
+play();
