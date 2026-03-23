ccccce018bbf226432a277f31ebbd1db79094f30
Chamisae812 <anjihoon7777@gmail.com>
2026-03-23 21:32:50 +0900

    ww

---
+++ 04-mysql/01_DOL.sql
@@ -0,0 +1,9 @@
+/* 
+  - 데이터(data) : 화면에 보이거나, 사용자가 입력하거나, 저장해야 하는 정보
+  - 데이터베이스(database) : 데이터를 저장하고 필요할 때 꺼내쓰는 공간
+  - DBMA(database management system) : 데이터베이스를 관리하는 프로그램
+  - RDBMS(relational database management system) : 관계형 데이터베이스 관리 시스템
+     예) MySQL, Oracle, PostgreSQL
+
+  - 엔티티(Entity) : 같은 성격을 가진 데이터를 묶어놓는 큰 주제
+*/
